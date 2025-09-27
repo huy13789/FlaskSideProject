409c23673c6ce8afa200f2181e4de0f5fbd54612
Admin <huy13789@gmail.com>
2025-09-27 13:47:35 +0700

    update models

--- Side_Project/my_flask_project/app/models/user.py
+++ Side_Project/my_flask_project/app/models/user.py
@@ -1,5 +1,5 @@
 class User:
     def __init__(self, username, password, email):
         self.username = username
-        self.password = password
+        self.password = password # Trong thực tế nên mã hóa mật khẩu!
         self.email = email
